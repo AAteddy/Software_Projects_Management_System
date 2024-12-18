03f8c323cf7397f9e8ee10c50729713e6e3c757e
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-18 17:20:28 +0300

    add jwt dependency and implement security configuration- AppConfig.java

---
+++ src/main/java/com/spms/config/JwtConstants.java
@@ -0,0 +1,8 @@
+package com.spms.config;
+
+public class JwtConstants {
+
+    public static final String SECRET_KEY = "+uTOv+AdyccUTw9M3YVNRIJ1BL6FKEw8HNQVF59J09q/qE254JKr0nC2rSzNCtZu";
+    public static final String JWT_HEADER = "Authorization";
+
+}
