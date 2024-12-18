02bea4eed53e9d56849232ee3b47a9bcb5a7b54a
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-18 14:15:01 +0300

    first commit - project initialization

---
+++ src/main/java/com/spms/SoftwareprojectmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.spms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareprojectmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareprojectmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spms/SoftwareprojectmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareprojectmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
