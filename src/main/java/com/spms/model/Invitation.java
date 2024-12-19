c3b9a0ada3f6332c2e3c821bf94c70d8f06c4080
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-19 17:07:22 +0300

    implement Invitation entity class for handling user invitations

---
+++ src/main/java/com/spms/model/Invitation.java
@@ -0,0 +1,25 @@
+package com.spms.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "invitations")
+public class Invitation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String token;
+
+    private String email;
+
+    private Long projectId;
+}
