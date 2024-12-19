34a0ab2163a9ea601c7c08d7ac8aa2a25bcb518c
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-19 17:01:32 +0300

    implement Message entity class for handling chat messages

--- src/main/java/com/spms/model/Chat.java
+++ src/main/java/com/spms/model/Chat.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 @NoArgsConstructor
@@ -22,4 +25,14 @@ public class Chat {
 
     @OneToOne
     private Project project;
+
+    @OneToMany(
+            mappedBy = "chat",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Message> messages;
+
+    @ManyToMany
+    private List<User> user = new ArrayList<>();
 }
---
+++ src/main/java/com/spms/model/Message.java
@@ -0,0 +1,31 @@
+package com.spms.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Chat chat;
+
+    @ManyToOne
+    private User sender;
+}
