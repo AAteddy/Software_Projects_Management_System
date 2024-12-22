1b48b2a9c1f8f11b25134ba90ae3bcd580c794f1
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-22 15:13:44 +0300

    implement inviteProject post method and InviteRequest class for handling sending an invitation to a team user & accepting user email & project Id respectively

--- src/main/java/com/spms/controller/ProjectController.java
+++ src/main/java/com/spms/controller/ProjectController.java
@@ -4,7 +4,9 @@ package com.spms.controller;
 import com.spms.model.Chat;
 import com.spms.model.Project;
 import com.spms.model.User;
+import com.spms.request.InviteRequest;
 import com.spms.response.ApiMessageResponse;
+import com.spms.service.InvitationService;
 import com.spms.service.ProjectService;
 import com.spms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class ProjectController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private InvitationService invitationService;
 
 
     @GetMapping
@@ -111,6 +116,20 @@ public class ProjectController {
         Chat chat = projectService.getChatByProjectId(projectId);
 
         return new ResponseEntity<>(chat, HttpStatus.OK);
+    }
+
+    @PostMapping("/invite")
+    public ResponseEntity<ApiMessageResponse> inviteProject(
+            @RequestBody InviteRequest request,
+            @RequestBody Project project,
+            @RequestHeader("Authorization") String jwt
+            ) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        invitationService.sendInvitation(request.getEmail(), request.getProjectId());
+        ApiMessageResponse response = new ApiMessageResponse("User Invitation Sent");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
