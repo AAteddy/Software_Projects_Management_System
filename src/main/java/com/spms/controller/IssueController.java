31b823f0a8cb4ecd24e37da10f2b7d1205c93637
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-25 15:37:46 +0300

    implement IssueController class and getIssueById method for handling getting an issue by its id request.

---
+++ src/main/java/com/spms/controller/IssueController.java
@@ -0,0 +1,37 @@
+package com.spms.controller;
+
+
+import com.spms.model.Issue;
+import com.spms.service.IssueService;
+import com.spms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/issues")
+public class IssueController {
+
+    @Autowired
+    private IssueService issueService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/{issueId}")
+    public ResponseEntity<Issue> getIssueById(
+            @PathVariable Long issueId
+    ) throws Exception {
+
+        return ResponseEntity.ok(issueService.getIssueById(issueId));
+    }
+
+
+}
