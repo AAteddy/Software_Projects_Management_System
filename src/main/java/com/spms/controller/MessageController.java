45466970deeee68ab5d129e50cb1b2c9973646bf
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-26 16:33:19 +0300

    implement sendMessage post method controller for handling sending a message

---
+++ src/main/java/com/spms/controller/MessageController.java
@@ -0,0 +1,52 @@
+package com.spms.controller;
+
+
+import com.spms.model.Chat;
+import com.spms.model.Message;
+import com.spms.model.User;
+import com.spms.request.CreateMessageRequest;
+import com.spms.service.MessageService;
+import com.spms.service.ProjectService;
+import com.spms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProjectService projectService;
+
+
+    @PostMapping("/send")
+    public ResponseEntity<Message> sendMessage(
+            @RequestBody CreateMessageRequest request
+            ) throws Exception {
+
+        User user = userService.findUserById(request.getSenderId());
+        if (user == null)
+            throw new Exception("User not found with user Id : " + request.getSenderId());
+
+        Chat chats = projectService.getProjectById(request.getProjectId()).getChat(); // this method should throw chatException if the chat is not found
+        if (chats == null)
+            throw new Exception("Chats not found");
+
+        Message sendMessage = messageService.sendMessage(
+                request.getSenderId(),
+                request.getProjectId(),
+                request.getContent());
+
+        return ResponseEntity.ok(sendMessage);
+    }
+}
