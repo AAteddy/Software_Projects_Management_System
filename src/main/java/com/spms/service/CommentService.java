dc534ff998b3fca5c9adf5630602cd5e748ff389
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-26 12:24:57 +0300

    implement findCommentsByIssueId method service that gets comments by issue Id

--- src/main/java/com/spms/service/CommentService.java
+++ src/main/java/com/spms/service/CommentService.java
@@ -10,6 +10,6 @@ public interface CommentService {
     Comment createComment(Long issueId, Long userId, String content) throws Exception;
 
     void deleteComment(Long commentId, Long userId) throws Exception;
-//
-//    List<Comment> findCommentByIssueId(Long issueId);
+
+    List<Comment> findCommentsByIssueId(Long issueId);
 }
--- src/main/java/com/spms/service/CommentServiceImpl.java
+++ src/main/java/com/spms/service/CommentServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -72,6 +73,11 @@ public class CommentServiceImpl implements CommentService {
         } else {
             throw new Exception("User does not have permission to delete this comment");
         }
+    }
+
+    @Override
+    public List<Comment> findCommentsByIssueId(Long issueId) {
+        return commentRepo.findByIssueId(issueId);
     }
 
 
