08b24321dc62686adbafc983b3590b56b7eb17a5
Tedros Alem <tedsaasfaha@gmail.com>
2024-12-26 13:45:10 +0300

    add MessageService interface, MesssageServiceImpl class, MessageRepo interface & implement sendMessage method service fir handling message sending & chat retrieval.

---
+++ src/main/java/com/spms/service/MessageServiceImpl.java
@@ -0,0 +1,45 @@
+package com.spms.service;
+
+
+import com.spms.model.Chat;
+import com.spms.model.Message;
+import com.spms.model.User;
+import com.spms.repository.MessageRepo;
+import com.spms.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ProjectService projectService;
+
+
+    @Override
+    public Message sendMessage(Long senderId, Long projectId, String content) throws Exception {
+        User sender = userRepo.findById(senderId)
+                .orElseThrow(() -> new Exception("User not found with Id : " + senderId));
+
+        Chat chat = projectService.getProjectById(projectId).getChat();
+
+        Message message = new Message();
+        message.setSender(sender);
+        message.setChat(chat);
+        message.setContent(content);
+        message.setCreatedAt(LocalDateTime.now());
+        Message savedMessage = messageRepo.save(message);
+
+        chat.getMessages().add(savedMessage);
+
+        return savedMessage;
+    }
+}
