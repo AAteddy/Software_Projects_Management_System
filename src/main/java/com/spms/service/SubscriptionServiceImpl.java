5cafba4793b23ddbba74de07d97263f8d0fabc1e
Tedros Alem <tedsaasfaha@gmail.com>
2025-01-03 13:26:09 +0300

    implement createSubscription service method for handling subscription creation and added the createSubscription service method for signedup user.

--- src/main/java/com/spms/service/SubscriptionServiceImpl.java
+++ src/main/java/com/spms/service/SubscriptionServiceImpl.java
@@ -1,11 +1,14 @@
 package com.spms.service;
 
 
+import com.spms.model.PlanType;
 import com.spms.model.Subscription;
 import com.spms.model.User;
 import com.spms.repository.SubscriptionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 
 @Service
@@ -14,9 +17,20 @@ public class SubscriptionServiceImpl implements SubscriptionService {
     @Autowired
     private SubscriptionRepo subscriptionRepo;
 
+    @Autowired
+    private UserService userService;
+
 
     @Override
     public Subscription createSubscription(User user) {
-        return null;
+        Subscription subscription = new Subscription();
+
+        subscription.setUser(user);
+        subscription.setSubscriptionStartDate(LocalDate.now());
+        subscription.setSubscriptionEndDate(LocalDate.now().plusMonths(12));
+        subscription.setPlanType(PlanType.FREE);
+        subscription.setValid(true);
+
+        return subscriptionRepo.save(subscription);
     }
 }
